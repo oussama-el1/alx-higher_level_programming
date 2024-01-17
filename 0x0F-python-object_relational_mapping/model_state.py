148f70fefdb852b40cb3e7c7786a632bd255db83
oussama-el1 <elhadraoui284@gmail.com>
2024-01-17 12:00:48 -0800

    add

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import declarative_base
 from sqlalchemy import column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 class State(Base):
