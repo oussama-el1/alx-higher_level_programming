de97e384f49a47ce2b5998a86004cb8eb61cb8ec
oussama-el1 <oussama284@example.com>
2024-01-17 21:03:43 +0100

    test

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-from sqlalchemy import column, String, Integer
-from sqlalchemy.ext.declarative import declarative_base
-=======
+
 #!/usr/bin/python3
 """
 Contains State class and Base, an instance of declarative_base()
@@ -12,14 +9,7 @@ from sqlalchemy.ext.declarative import declarative_base
 meta = MetaData()
 Base = declarative_base(metadata=meta)
 
->>>>>>> e42e250bd46dc59dc1e121c8e16a57af56ed7fe6
-
 class State(Base):
     __tablename__ = 'states'
-<<<<<<< HEAD
-    id = column(Integer, primary_key=True)
-    name = column(String(length=100), nullable = False)
-=======
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable = False)
->>>>>>> e42e250bd46dc59dc1e121c8e16a57af56ed7fe6
